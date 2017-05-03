504a8b6d6ba1dbb24a4b693003df01e4f2ab5d9e
Oscar Karlsson <oscar.karlsson@schibsted.se>
2017-05-03 09:06:03 +0200

    Initial Commit

---
+++ xcodes-personal-assistant/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xcodes-personal-assistant
+//
+//  Created by Oscar Karlsson on 2017-05-03.
+//  Copyright © 2017 Oscar Karlsson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
